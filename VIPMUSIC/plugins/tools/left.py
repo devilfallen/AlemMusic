892f2e546bdf73327e0b3c2b9771bf1e299d94c1
AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>
2024-05-28 02:17:11 +0300

    Update left.py

--- VIPMUSIC/plugins/tools/left.py
+++ VIPMUSIC/plugins/tools/left.py
@@ -31,11 +31,11 @@ import random
 import asyncio
 
 random_photo = [
-    "https://telegra.ph/file/1949480f01355b4e87d26.jpg",
-    "https://telegra.ph/file/3ef2cc0ad2bc548bafb30.jpg",
-    "https://telegra.ph/file/a7d663cd2de689b811729.jpg",
-    "https://telegra.ph/file/6f19dc23847f5b005e922.jpg",
-    "https://telegra.ph/file/2973150dd62fd27a3a6ba.jpg",
+    "https://te.legra.ph/Alem-Music-05-27",
+    "https://te.legra.ph/Alem-Music-05-27",
+    "https://te.legra.ph/Alem-Music-05-27",
+    "https://te.legra.ph/Alem-Music-05-27",
+    "https://te.legra.ph/Alem-Music-05-27",
 ]
 
 bg_path = "VIPMUSIC/assets/userinfo.png"
