c01b2f9b14fc80bd402bb54ad9bdacfd9ec26a10
AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>
2024-05-28 02:12:25 +0300

    Update bot_add.py

--- VIPMUSIC/plugins/tools/bot_add.py
+++ VIPMUSIC/plugins/tools/bot_add.py
@@ -17,11 +17,11 @@ from VIPMUSIC.utils.database import get_assistant
 
 
 photo = [
-    "https://telegra.ph/file/1949480f01355b4e87d26.jpg",
-    "https://telegra.ph/file/3ef2cc0ad2bc548bafb30.jpg",
-    "https://telegra.ph/file/a7d663cd2de689b811729.jpg",
-    "https://telegra.ph/file/6f19dc23847f5b005e922.jpg",
-    "https://telegra.ph/file/2973150dd62fd27a3a6ba.jpg",
+    "https://te.legra.ph/Alem-Music-05-27",
+    "https://te.legra.ph/Alem-Music-05-27",
+    "https://te.legra.ph/Alem-Music-05-27",
+    "https://te.legra.ph/Alem-Music-05-27",
+    "https://te.legra.ph/Alem-Music-05-27",
 ]
 
 from strings.__init__ import LOGGERS
