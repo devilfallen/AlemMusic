5a61c770255986a4cf2d6cc8cd8202ab64b9f670
AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>
2024-05-28 01:32:39 +0300

    Update start.py

--- VIPMUSIC/cplugin/start.py
+++ VIPMUSIC/cplugin/start.py
@@ -40,9 +40,9 @@ SPAM_WINDOW_SECONDS = 5
 
 
 YUMI_PICS = [
-    "https://telegra.ph/file/3134ed3b57eb051b8c363.jpg",
-    "https://telegra.ph/file/5a2cbb9deb62ba4b122e4.jpg",
-    "https://telegra.ph/file/cb09d52a9555883eb0f61.jpg",
+    "https://te.legra.ph/Alem-Music-05-27",
+    "https://te.legra.ph/Alem-Music-05-27",
+    "https://te.legra.ph/Alem-Music-05-27",
 ]
 
 
